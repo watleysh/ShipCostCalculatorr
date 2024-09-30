43ddfd8e09d285c13c4cb00e4c1399b3e505c76f
watleysh <watleysh@mail.uc.edu>
2024-09-29 23:13:40 -0400

    final  commit

--- ShipCostCalculator.java
+++ ShipCostCalculator.java
@@ -9,5 +9,15 @@ public class ShipCostCalculator {
         System.out.println ("What is the price of the item?");
         itemPrice = scanner.nextDouble();
 
+        if (itemPrice >= FREE_SHIPPING_REACHED) {
+            System.out.println("You have free shipping!");
+        } else {
+            shippingCost =itemPrice * .02;
+            System.out.println("The shipping is " + shippingCost + ", and your total is " + (itemPrice + shippingCost) );
 
+        }
+
+
+        scanner.close();
+    }
     }
